2bc2edef8951aac7cc73a823c7174c338a3f9175
Alexander Duboysky <alexander.duboysky@silkdata.ai>
2024-11-13 21:12:49 +0100

    finish cv page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
-
-import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		icon({
-			iconDir: 'public/icons'
-		})
-	]
+	integrations: [tailwind()]
 })
---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const TELEGRAM_LINK = 'https://t.me/petrzelinsky'
+
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/petrzelinsky/'
+
+export const CV_LINK = '/cv.pdf'
